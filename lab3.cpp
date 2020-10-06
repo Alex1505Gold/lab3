72ccc0fc471ce34825eeb030975ba5d1c1c569d1
Alex1505Gold <49805582+Alex1505Gold@users.noreply.github.com>
2020-10-06 15:58:04 +0300

    Add files via upload

--- lab3.cpp
+++ lab3.cpp
@@ -49,15 +49,15 @@ int main()
     std::cout << "vvedi massiv" << std::endl;
     for (int i = 0; i < m; ++i) {
         std::cin >> a;
-        vec1.insert(vec1.end(), a);
+        vec1.push_back(a);
     }
-    for (int i = 0; i < m; ++i) std::cout << vec1[i] << " ";
+    for (int i : vec1) std::cout << i << " ";
     std::cout << std::endl;
-    vec1.insert(vec1.end(), 8); //8 variant
-    for (int i = 0; i < vec1.size(); ++i) std::cout << vec1[i] << " ";
+    vec1.push_back(8); //8 variant
+    for (int i : vec1) std::cout << i << " ";
     std::cout << std::endl;
     sum = 0;
-    for (int i = 0; i < vec1.size(); ++i) sum += vec1[i];
+    for (int i : vec1) sum += i;
     std::cout << "summa vectora ravna " << sum << std::endl;
     int nomer = -1;
     for (int i = 0; i < vec1.size(); ++i) {
@@ -67,7 +67,7 @@ int main()
         }
     }
     if (nomer != -1) vec1.erase(vec1.begin()+nomer);
-    for (int i = 0; i < vec1.size(); ++i) std::cout << vec1[i] << " ";
+    for (int i : vec1) std::cout << i << " ";
     std::cout << std::endl;
     //zadanie 4
     std::cout << "vvedi stroky" << std::endl;
@@ -84,15 +84,29 @@ int main()
     std::cin >> l1 >> l2;
     std::cout << str1.substr(l1, l2 - l1 + 1) << std::endl;
     std::cout << "vvedi zameny" << std::endl;
-    std::string str2 = "Can you can a can as a canner can can a can";
+    std::string str2 = "Can you can a can as a canner can can a can?";
     std::string str3;
+    std::string strH = "TMP9";
     std::cin.ignore(10, '\n');
     std::getline(std::cin, str3);
     std::string strc = "can";
     while (str2.find(strc) != std::string::npos) {
         auto pos = str2.find(strc);
-        str2.erase(pos, strc.size());
-        str2.insert(pos, str3);
+        std::cout <<pos << " " << str2[pos + strc.size()] << std::endl;
+        if ((str2[pos + strc.size()] == ' ') || (str2[pos + strc.size()] == '?')) {
+            str2.erase(pos, strc.size());
+            str2.insert(pos, str3);
+        }
+        else {
+            str2.erase(pos, strc.size());
+            str2.insert(pos, strH);
+        }
+    }
+    while (str2.find(strH) != std::string::npos) {
+        auto pos = str2.find(strH);
+        //std::cout << pos << " " << str2[pos + strH.size()] << std::endl;
+            str2.erase(pos, strH.size());
+            str2.insert(pos, strc);
     }
     strc = "Can";
     if ((str3[0] >= 97) && (str3[0] <= 122)) str3[0] -= 32; // делаем букву заглавной тк она будет в начале предложения
